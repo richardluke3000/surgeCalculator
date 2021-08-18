0a356fd04d65aa147b62d13a97432da0ed12666d
PrinceMwase <princefranklinemwase@gmail.com>
2021-08-18 23:36:37 +0200

    refactored working prototype

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // for excel
 var XLSX = require('xlsx')
 
-const {BrowserWindow, Menu, app, shell, dialog, ipcMain} = require('electron')
+const { BrowserWindow, Menu, app, shell, dialog, ipcMain } = require('electron')
 
 // MENU TEMPLATE
 let template = [{
@@ -71,215 +71,239 @@ app.on('ready', () => {
 // code. You can also put them in separate files and require them here.
 
 // save dialog 
-ipcMain.on('destination', (event, args) => {
-  let result = dialog.showOpenDialog({
-    properties: ['openDirectory']
-  })
-  event.sender.send('result', 'selecting directory...')
+destination()
 
-  result.then((value) => {
+extract()
+
+
+open_file_dialog()
+
+filesFolder()
+
+consolidate()
+
+
+function consolidate() {
+  ipcMain.on('consolidate', (event, dir, files, destination, startDate, sheetToConsolidate) => {
+
+    let arg = dir
+
+    // collumn to search for a date
+    let dateCollumn
+
     loadEvent(event)
 
-    if (!value.canceled) {
+    let array_of_sheets = ["Active Index Testing ", "OPD Screening", "Missed Appointments ", "Defaulter Q1"]
 
-      event.sender.send('selected-destination', value.filePaths)
+    let finalsheet = []
+
+    let wb = XLSX.utils.book_new()
+    wb.Props = {
+      Title: "surge cascade " + startDate,
+      Subject: "Output",
+      Author: "UI-HEXED",
+      CreatedDate: new Date(),
     }
 
-  })
-})
+    array_of_sheets.forEach(element => {
 
-ipcMain.on('extract', (event, arg) => {
+      sheetToConsolidate = element
 
-  console.log(arg[2]);
-
-  // first read fie
-  let sheet = XLSX.readFile(arg[0], {  sheets: [arg[2]] })
+      files.forEach((element, index) => {
 
 
-  // creating a new workbok
-  var wb = XLSX.utils.book_new()
+        let path = `${arg}\\${element}`
 
-  // get the workbook data
-  let data = sheet.Sheets[arg[2]]
-
-  XLSX.utils.book_append_sheet(wb, data, arg[2])
-
-  // write to new file
-  XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
-
-  XLSX.writeFile(wb, `${arg[1]}`);
-
-  event.sender.send('result', 'finished Extracting sheet')
-
-
-})
-
-
-ipcMain.on('open-file-dialog', (event, arg) => {
-
-
-  let result = dialog.showOpenDialog({
-    properties: ['openFile']
-  })
-  result.then((value) => {
-
-    if (!value.canceled) {
-
-      loadEvent(event)
-
-      var workbook = XLSX.readFile(value.filePaths[0]);
-      var sheet_name_list = workbook.SheetNames;
-      event.sender.send('sheets', sheet_name_list)
-
-      event.sender.send('selected-directory', value.filePaths)
-    }
-
-  })
-
-})
-
-ipcMain.on('filesFolder', (event, arg)=>{
-  let result = dialog.showOpenDialog({
-    properties: ['openDirectory']
-  })
-
-  result.then((value) => {
-    
-    if (!value.canceled) {
-
-      loadEvent(event)
-
-      
-
-
-      //variable to contain files in the directory
-      let newFiles = [];
-
-
-    fs.readdir( value.filePaths[0], function(err, files){
-      
-      if (err) {
-        // do stuff to handle the error
-        
-        return event.sender.send('result', 'unable to do necessary stuff : ' + err)
-
-      }
-      let patt = new RegExp("^[A-z].*.xlsx")
-
-      
-
-      files.forEach(file => {
-
-        const filtered = patt.exec(file)
-
-
-        if( filtered ){
-          newFiles.push( filtered[0] )
+        if (sheetToConsolidate === 'Defaulter Q1') {
+          dateCollumn = 'F'
+        } else {
+          dateCollumn = 'E'
         }
-        
-      });
-      event.sender.send('selected-consolidate', value.filePaths, newFiles)
-      event.sender.send('result', `Found ${newFiles.length} files`)
-    } )
-      
-    }
+
+        var sheet = XLSX.readFile(path, { sheets: sheetToConsolidate })
+
+
+        let data = sheet.Sheets[sheetToConsolidate]
+
+
+        XLSX.utils.sheet_to_json(data)
+
+        // parsing date to be compatible with workboook dates
+        let check = Date.parse(startDate).toString().substr(0, 5)
+        let check2 = parseInt(check) + 6
+        let check2string = check2.toString()
+
+
+        let taken = []
+
+        // this will check every key / or cell in the sheeet
+        for (const key in data) {
+
+          if (key[0] == dateCollumn && (Date.parse(data[key].w).toString().substr(0, 5) == check || Date.parse(data[key].w).toString().substr(0, 5) == check2string)) {
+            let row = key.substr(1, 100)
+
+
+            for (const key in data) {
+
+              if (row == key.substr(1, 100)) {
+                // this if statement will check date collumn to output correct date
+                if (key[0] == dateCollumn) {
+                  taken.push(data[key].w)
+                }
+                else {
+                  taken.push(data[key].v)
+                }
+
+              }
+
+            }
+
+          }
+          if (taken.length > 0) {
+            finalsheet.push(taken)
+            taken = []
+          }
+        }
+
+      })
+
+      let sheet_of_all_files = XLSX.utils.aoa_to_sheet(finalsheet)
+
+      finalsheet = []
+
+      XLSX.utils.book_append_sheet(wb, sheet_of_all_files, sheetToConsolidate)
+
+    })
+
+    XLSX.write(wb, { bookType: 'xlsx', type: 'binary' })
+
+    XLSX.writeFile(wb, destination)
+
+    event.sender.send('result', 'finished consolidating')
+
 
   })
-})
-
-ipcMain.on('consolidate', (event ,  dir, files , destination, startDate, sheetToConsolidate )=>{
-  
-  let arg = dir
-
-  // collumn to search for a date
-  let dateCollumn;
-
-  loadEvent(event)
-
-  let array_of_sheets = ["Active Index Testing ", "OPD Screening", "Missed Appointments ", "Defaulter Q1"]
-
-  let finalsheet = []
-
-  let wb = XLSX.utils.book_new();
-  wb.Props = {
-    Title: "surge cascade " + startDate ,
-    Subject: "Output",
-    Author: "UI-HEXED",
-    CreatedDate: new Date(),
 }
 
-  array_of_sheets.forEach(element => {
-    
-    sheetToConsolidate = element
+function filesFolder() {
+  ipcMain.on('filesFolder', (event) => {
+    let result = dialog.showOpenDialog({
+      properties: ['openDirectory']
+    })
 
-    files.forEach((element, index) => {
-     
+    result.then((value) => {
 
-      let path = `${arg}\\${element}`
-  
-      if( sheetToConsolidate === 'Defaulter Q1' ){
-        dateCollumn = 'F'
-      }else{
-        dateCollumn = 'E'
-      }
-  
-      var sheet = XLSX.readFile( path, {sheets: sheetToConsolidate } );
-     
-  
-      let data = sheet.Sheets[sheetToConsolidate]
-    
-  
-      XLSX.utils.sheet_to_json(data)
-  
-      // parsing date to be compatible with workboook dates
-      let check = Date.parse(startDate).toString().substr(0,5)
-      let check2 = parseInt(check) + 6
-      let check2string = check2.toString()
-     
-      
-      let taken = [] ;
-     
-      // this will check every key / or cell in the sheeet
-      for (const key in data) {
-       
-  
-        if( key[0] == dateCollumn && ( Date.parse(data[key].w).toString().substr(0,5) ==  check ||  Date.parse(data[key].w).toString().substr(0,5) == check2string )){
-          let row = key.substr(1,100);
-          
-          
-            for (const key in data) {
-              
-              if( row == key.substr(1,100)   )
-                taken.push( data[key].v )
-              
+      if (!value.canceled) {
+
+        loadEvent(event)
+
+
+
+
+        //variable to contain files in the directory
+        let newFiles = []
+
+
+        fs.readdir(value.filePaths[0], function (err, files) {
+
+          if (err) {
+            // do stuff to handle the error
+            return event.sender.send('result', 'unable to do necessary stuff : ' + err)
+
+          }
+          let patt = new RegExp("^[A-z].*.xlsx")
+
+
+
+          files.forEach(file => {
+
+            const filtered = patt.exec(file)
+
+
+            if (filtered) {
+              newFiles.push(filtered[0])
             }
-  
-        }
-        if ( taken.length > 0  ){
-          finalsheet.push(taken)
-          taken = []
-        }
+
+          })
+          event.sender.send('selected-consolidate', value.filePaths, newFiles)
+          event.sender.send('result', `Found ${newFiles.length} files`)
+        })
+
       }
-  
-    });
 
-    let sheet_of_all_files = XLSX.utils.aoa_to_sheet(finalsheet);
+    })
+  })
+}
 
-    finalsheet = []
-
-    XLSX.utils.book_append_sheet(wb, sheet_of_all_files, sheetToConsolidate)
-
-  });
-
-  XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
-
-  XLSX.writeFile(wb, destination);
-  
-  event.sender.send('result', 'finished consolidating')
+function open_file_dialog() {
+  ipcMain.on('open-file-dialog', (event) => {
 
 
-})
+    let result = dialog.showOpenDialog({
+      properties: ['openFile']
+    })
+    result.then((value) => {
 
+      if (!value.canceled) {
+
+        loadEvent(event)
+
+        var workbook = XLSX.readFile(value.filePaths[0])
+        var sheet_name_list = workbook.SheetNames
+        event.sender.send('sheets', sheet_name_list)
+
+        event.sender.send('selected-directory', value.filePaths)
+      }
+
+    })
+
+  })
+}
+
+function extract() {
+  ipcMain.on('extract', (event, arg) => {
+
+    // first read fie
+    let sheet = XLSX.readFile(arg[0], { sheets: [arg[2]] })
+
+
+    // creating a new workbok
+    var wb = XLSX.utils.book_new()
+
+    // get the workbook data
+    let data = sheet.Sheets[arg[2]]
+
+    XLSX.utils.book_append_sheet(wb, data, arg[2])
+
+    // write to new file
+    XLSX.write(wb, { bookType: 'xlsx', type: 'binary' })
+
+    XLSX.writeFile(wb, `${arg[1]}`)
+
+    event.sender.send('result', 'finished Extracting sheet')
+
+
+  })
+}
+
+function destination() {
+  ipcMain.on('destination', (event) => {
+    let result = dialog.showOpenDialog({
+      properties: ['openDirectory']
+    })
+    event.sender.send('result', 'selecting directory...')
+
+    result.then((value) => {
+      loadEvent(event)
+
+      if (!value.canceled) {
+
+        event.sender.send('selected-destination', value.filePaths)
+      }
+
+    })
+  })
+}
 
 function loadEvent(event) {
 
